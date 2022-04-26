9d29df487e3c8ac9928ec8e65f9a626ffcc8102c
kinglylam <99560872+kinglylam@users.noreply.github.com>
2022-04-26 17:02:07 +0100

    Update database.js

--- scr/config/database.js
+++ scr/config/database.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
-require("dotenv").config();
-const PORT = process.env.PORT ?? 5000;
 
-const connectDatabase = async (app) => {
-    try {
-        await mongoose.connect(process.env.DB_URL, () => {
-            console.log("connected to database...");
-            app.listen(PORT, () => 
-               console.log("server started on port:${PORT}..")
-            );
-        });
-    } catch (error) {
-        console.log(error);
-    }
-};
-module.exports = connectDatabase;
+
+
+
+
+const connectDatabase=async(URL)=>{
+    await mongoose.connect(URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    });
+}
+
+module.exports= connectDatabase;
