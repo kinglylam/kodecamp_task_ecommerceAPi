2a89c70965e83d94e997f2753400affdac5fb56d
kinglylam <99560872+kinglylam@users.noreply.github.com>
2022-04-26 20:21:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/product', productRouter);
 
 console.log("waiting for database");
 
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT || 4000;
 const con = async () => {
   try {
     await connectDatabase(process.env.DB_URI);
