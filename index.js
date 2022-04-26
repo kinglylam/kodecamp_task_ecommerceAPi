8162bf29d1a7a7aa594eed91a78cdacef2a9f630
kinglylam <99560872+kinglylam@users.noreply.github.com>
2022-04-26 17:13:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser =require("body-parser");
+require("dotenv").config();
 const app = express();
 
 
@@ -7,10 +7,8 @@ const  connectDatabase = require("./scr/config/database");
 
 const productRouter = require('./scr/api/routes/product');
 
-connectDatabase(app);
-
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/", (req, res) => {
     res.send(" <h1> ecommerce API </h1>");
@@ -20,3 +18,18 @@ app.use('/product', productRouter);
 
 console.log("waiting for database");
 
+const PORT = process.env.PORT ?? 5000;
+const con = async () => {
+  try {
+    await connectDatabase(process.env.DB_URI);
+    app.listen(PORT, () => {
+      console.log(`Server started at port ${PORT}...`);
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
+
+};
+
+con();
